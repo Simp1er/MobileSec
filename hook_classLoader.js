bfa372bde1c8376df170c884cd7b35d963039f94
Simp1er <simp1er_sec@163.com>
2021-07-28 16:21:55 +0800

    2021-07-28 fix bug

--- hook_classLoader.js
+++ hook_classLoader.js
@@ -13,7 +13,7 @@ function hook() {
                 next: {
                     returnType: 'java.lang.Boolean',
                     // returnType: 'java.lang.Boolean',
-                      argumentTypes: [],
+                    //   argumentTypes: [],
                     argumentTypes: [],
                     implementation: function () {
                         console.log('call next')
@@ -32,58 +32,6 @@ function hook() {
         pathLoader.parent.value = dexLoader
         dexLoader.parent.value = parent
 
-
-    })
-
-}
-
-function hook1() {
-    Java.perform(function () {
-        // var Six = Java.use('com.dta.test.frida.activity.SixthActivity')
-        Java.registerClass({
-            name: 'com.dta.test.frida.activity.RegisterClass',
-            // implements: [Six],
-            // superClass: Java.use('java.lang.Object'),
-            methods: {
-                $init: function () {
-                    console.log('Constructor called');
-                },
-                next: {
-                    returnType: 'java.lang.Boolean',
-                    // returnType: 'java.lang.Boolean',
-                      argumentTypes: [],
-                    argumentTypes: [],
-                    implementation: function () {
-                        console.log('call next')
-                        return Java.use('java.lang.Boolean').$new('true');
-                        // return Java.use('java.lang.Object').$new('true');
-                        // return true;
-                    },
-                },
-            }
-        });
-        Java.enumerateClassLoaders({
-            onMatch: function (laoder) {
-                try {
-                    laoder.loadClass('com.dta.test.frida.activity.RegisterClass')
-                    console.log('loader', laoder)
-                    Java.ClassFactory.loader = laoder
-                } catch {
-                    console.log('except')
-                }
-            },
-            onComplete: function () {
-                console.log('search complete')
-            }
-
-        })
-        var Class = Java.use('java.lang.Class')
-        Class.forName.overload('java.lang.String', 'boolean', 'java.lang.ClassLoader').implementation = function (str, init, loader) {
-            console.log('loader', loader)
-            console.log('className', str)
-            console.log('iniit', init)
-            return this.forName(str, init, Java.ClassFactory.loader)
-        }
 
     })
 
