ae6252a2ca4b1494ce0ecb445e9b721715c40178
Simp1er <Simp1er@users.noreply.github.com>
2022-02-25 16:29:35 +0800

    参考yang神获取prettyMethod函数的方法修正代码
    解决在未获取得到PrettyMethod函数地址的情况下无法打印函数名的问题

--- hook_RegisterNative.js
+++ hook_RegisterNative.js
@@ -2,12 +2,42 @@
 仅在Android 8.1下测试成功，其他版本可能需要重新修改适配
 */
 
-var ArtMethod_PrettyMethod
-function readStdString(str) {
-    if ((str.readU8() & 1) === 1) { // size LSB (=1) indicates if it's a long string
-        return str.add(2 * Process.pointerSize).readPointer().readUtf8String();
+const STD_STRING_SIZE = 3 * Process.pointerSize;
+class StdString {
+    constructor() {
+        this.handle = Memory.alloc(STD_STRING_SIZE);
     }
-    return str.add(1).readUtf8String();
+
+    dispose() {
+        const [data, isTiny] = this._getData();
+        if (!isTiny) {
+            Java.api.$delete(data);
+        }
+    }
+
+    disposeToString() {
+        const result = this.toString();
+        this.dispose();
+        return result;
+    }
+
+    toString() {
+        const [data] = this._getData();
+        return data.readUtf8String();
+    }
+
+    _getData() {
+        const str = this.handle;
+        const isTiny = (str.readU8() & 1) === 0;
+        const data = isTiny ? str.add(1) : str.add(2 * Process.pointerSize).readPointer();
+        return [data, isTiny];
+    }
+}
+
+function prettyMethod(method_id, withSignature) {
+    const result = new StdString();
+    Java.api['art::ArtMethod::PrettyMethod'](result, method_id, withSignature ? 1 : 0);
+    return result.disposeToString();
 }
 function attach(addr) {
     Interceptor.attach(addr, {
@@ -19,9 +49,8 @@ function attach(addr) {
             modulemap.update()
             var module = modulemap.find(retval)
             var string = Memory.alloc(0x100)
-            ArtMethod_PrettyMethod(string, this.arg0, 1)
             if (module != null) {
-                console.log('<' + module.name + '> method_name =>', readStdString(string), ',offset=>', ptr(retval).sub(module.base), ',module_name=>', module.name)
+                console.log('<' + module.name + '> method_name =>',prettyMethod(this.arg0,1), ',offset=>', ptr(retval).sub(module.base), ',module_name=>', module.name)
             }else{
                 console.log('<anonymous> method_name =>', readStdString(string), ', addr =>', ptr(retval))
             }
@@ -33,9 +62,6 @@ function hook_RegisterNative() {
     var libart = Process.findModuleByName('libart.so')
     var symbols = libart.enumerateSymbols()
     for (var i = 0; i < symbols.length; i++) {
-        if (symbols[i].name.indexOf('PrettyMethod') > -1 && symbols[i].name.indexOf('ArtMethod') > -1 && symbols[i].name.indexOf("Eb") >= 0) {
-            ArtMethod_PrettyMethod = new NativeFunction(symbols[i].address, "void", ['pointer', "pointer", "bool"])
-        }
         if (symbols[i].name.indexOf('RegisterNative') > -1 && symbols[i].name.indexOf('ArtMethod') > -1 && symbols[i].name.indexOf('RuntimeCallbacks') < 0) {
             attach(symbols[i].address)
         }
